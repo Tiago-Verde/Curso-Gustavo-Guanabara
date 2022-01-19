018192361754af44027f7543f34293ac3ac0b45e
Tiago-Verde <vutu.goes@gmail.com>
2022-01-18 21:48:59 -0300

    Exercicio 36

--- Python - Exercicios/exercicios/Exercicio Mundo 2/ex036.py
+++ Python - Exercicios/exercicios/Exercicio Mundo 2/ex036.py
@@ -6,5 +6,15 @@ valorCasa = float(input('Qual o valor da casa que você está comprando: '))
 salario = float(input('Qual o seu salario? '))
 pagamento = int(input('Em quantos anos você pretende realizar o pagamento? '))
 
-numeroParcelas = pagamento / 12
+numeroParcelas = pagamento * 12
 valorParcelas = valorCasa / numeroParcelas
+valorLimite = (salario * 30 / 100) 
+
+if valorParcelas > valorLimite:
+    print('Infelizmente seu emprestimos foi negado')
+else:
+    print('Seu emprestimo foi autorizado!!!!')
+    print('O valor de suas parcelas ficaram em R$ {:.2f}'.format(valorParcelas))
+print('Valor do imovel R$ {:.2f}'.format(valorCasa))
+print('Numero de parcelas do financiamento: {}'.format(numeroParcelas))
+
